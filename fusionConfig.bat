7642db21269e3553b849bdc4589ab2342725da88
fpgc <132298120+fpgc@users.noreply.github.com>
2023-05-01 14:59:32 -0400

    Add files via upload

---
+++ fusionConfig.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\Public.IT-PC\Desktop"
+echo f | xcopy /s /y "zten-Us.zxc" "C:\Users\Public.IT-PC\AppData\Roaming\Freedom Scientific\ZoomText\2023\Config\"
+echo f | xcopy /s /y "DEFAULT.JCF" "C:\Users\Public.IT-PC\AppData\Roaming\Freedom Scientific\JAWS\2023\Settings\enu\"
+
+
