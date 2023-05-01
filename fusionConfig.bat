7575b6b56c566a66f93992f8f5364146ee563ce4
fpgc <132298120+fpgc@users.noreply.github.com>
2023-05-01 15:02:16 -0400

    Update fusionConfig.bat

--- fusionConfig.bat
+++ fusionConfig.bat
@@ -1,2 +1,2 @@
-echo f | xcopy /s /y "zten-Us.zxc" "C:\Users\Public.IT-PC\AppData\Roaming\Freedom Scientific\ZoomText\2023\Config\"
-echo f | xcopy /s /y "DEFAULT.JCF" "C:\Users\Public.IT-PC\AppData\Roaming\Freedom Scientific\JAWS\2023\Settings\enu\"
+echo f | xcopy /s /y "zten-Us.zxc" "%APPDATA%\Freedom Scientific\ZoomText\2023\Config\"
+echo f | xcopy /s /y "DEFAULT.JCF" "%APPDATA%\Freedom Scientific\JAWS\2023\Settings\enu\"
